e4fda8144e7e6e020c224f23c926b22f287777ba
Dave Fisher <dave.j.fish@gmail.com>
2022-10-07 16:10:44 -0700

    getting words from pics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
+import Tesseract from 'tesseract.js';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+
+    return Tesseract.recognize(
+      file,
+      'eng',
+      // { logger: m => console.log(m) }
+    ).then(({ data: { text } }) => {
+      setData(text);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='file-upload'>
+        <input onChange={handleChange} type='file'></input>
+        <p>{data}</p>
+      </div>
+      
     </div>
   );
 }
